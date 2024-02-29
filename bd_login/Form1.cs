5895dc4ff8a7f6c805313f87a5f75363a3883cb3
luquila2 <lucas.aquila.silva@hotmail.com>
2024-02-29 15:23:24 -0300

    ok

---
+++ bd_login/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bd_login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("pesquisar por nome de usuario!");
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("login realizado com sucesso!");
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                // Se o RadioButtonusuario for selecionado
+                MessageBox.Show("RadioButtonusuario está selecionado!");
+            }
+            else
+            {
+                // Se o RadioButtonusuario for cancelado
+                MessageBox.Show("RadioButtonusuario está desabilitado!");
+            }
+        }
+    }
+}
